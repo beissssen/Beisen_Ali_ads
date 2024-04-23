cd2763ae87aff3cd8cc7fe4e5f3ed15ca0fe325d
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-23 17:34:56 +0500

    Update Main.java

--- assighnment2_ads_Beisen_Ali/src/Main.java
+++ assighnment2_ads_Beisen_Ali/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyLinkedList myLinkedList = new MyLinkedList<>();
+
+        myLinkedList.addElement(1);
+        myLinkedList.addElement(2);
+        myLinkedList.addElement(3);
+        myLinkedList.addElement(4);
+        myLinkedList.addElement(5);
     }
 }
