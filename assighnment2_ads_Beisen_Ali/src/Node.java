bc87ca867d61a9de88b76b0f5db398961a2035b6
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-23 22:30:20 +0500

    Update Node.java

--- assighnment2_ads_Beisen_Ali/src/Node.java
+++ assighnment2_ads_Beisen_Ali/src/Node.java
@@ -1,9 +1,16 @@
 public class Node<T> {
-    T data;
+    T data;//stores data in node
     Node next;
 
     public Node(T data) {
         this.data = data;
         next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+    public T getNext() {
+        return data;
     }
 }
