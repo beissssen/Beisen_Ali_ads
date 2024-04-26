29802b2d384963293cd6de03f6ddd89f3efc383b
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-26 18:59:53 +0500

    Update List.java

--- assighnment2_ads_Beisen_Ali/src/List.java
+++ assighnment2_ads_Beisen_Ali/src/List.java
@@ -1,3 +1,29 @@
-public interface List<T> extends Iterable<T>{
+import java.util.Iterator;
+
+
+public interface MyList<T> {
     void addElement(T data);
+
+    void setElement(int index, T element);
+
+    void add(T element);
+    void add(T element, int index);
+    void addFirst(T element);
+    void addLast(T element);
+    void set(T element, int index);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(T element);
+    int lastIndexOf(T element);
+    boolean exists(T element);
+    T[] toArray();
+    void clear();
+    int size();
+
+    Iterator<T> iterator();
 }
