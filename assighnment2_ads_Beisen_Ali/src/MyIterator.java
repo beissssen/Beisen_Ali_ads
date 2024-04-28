2b5f018a5523beb8ccc2d03a61481251aafca070
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-28 15:36:56 +0500

    Update MyIterator.java

--- assighnment2_ads_Beisen_Ali/src/MyIterator.java
+++ assighnment2_ads_Beisen_Ali/src/MyIterator.java
@@ -1,11 +1,19 @@
 public class MyIterator<T extends Comparable<T>> {
+    /* Iterator class to iterate over elements in a linked list.
+     * It provides methods to iterate over elements in a linked list
+     * and throwing an exception if it is need
+    */
+    
     private Node<T> currentNode;
+
     public MyIterator(MyLinkedList<T> list) {
         currentNode = list.getHead();
     }
+
     public boolean hasNext() {
         return currentNode != null;
     }
+
     public T next() {
         if (!hasNext()) {
             throw new IllegalStateException("No next element available");
