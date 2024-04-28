f20bb8398293ec3a75905d499e800cf3d64a25ab
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-28 15:11:01 +0500

    Update MyQueue.java

--- assighnment2_ads_Beisen_Ali/src/MyQueue.java
+++ assighnment2_ads_Beisen_Ali/src/MyQueue.java
@@ -1,4 +1,6 @@
-public class MyQueue<T> {
+package classes;
+
+public class MyQueue<T extends Comparable<T>> implements DataType<T> {
     private MyLinkedList<T> queue;
 
     public MyQueue() {
@@ -21,4 +23,31 @@ public class MyQueue<T> {
     public boolean isEmpty() {
         return queue.getSize() == 0;
     }
+
+    public void push(T e) {
+        MyLinkedList<T> linkedList = new MyLinkedList<>();
+        linkedList.addFirst(e);
+    }
+
+
+
+    public T pop() {
+        MyLinkedList<T> linkedList = new MyLinkedList<>();
+        T t = linkedList.getLast();
+        linkedList.removeLast();
+        return t;
+    }
+
+
+    public T get() {
+        MyLinkedList<T> linkedList = new MyLinkedList<>();
+        return linkedList.getLast();
+    }
+
+
+    public int size() {
+        MyLinkedList<T> linkedList = new MyLinkedList<>();
+        return linkedList.size();
+    }
+
 }
