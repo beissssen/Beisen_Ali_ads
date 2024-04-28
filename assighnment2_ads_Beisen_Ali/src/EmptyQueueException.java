fe9828063ffdd80fdaae426774c380fb34922d90
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-28 15:30:39 +0500

    Update EmptyQueueException.java

--- assighnment2_ads_Beisen_Ali/src/EmptyQueueException.java
+++ assighnment2_ads_Beisen_Ali/src/EmptyQueueException.java
@@ -1,4 +1,6 @@
 public class EmptyQueueException extends RuntimeException {
+    /* Exception class for the moments where a queue is empty and an operation requiring an element from the queue is attempted
+    */
     public EmptyQueueException(String message) {
         super(message);
     }
