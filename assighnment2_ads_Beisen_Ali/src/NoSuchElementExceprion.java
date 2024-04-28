e5c1dc08510e13c39873d997ffc6991bcf8473e3
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-28 15:13:49 +0500

    Update Exceprion.java

--- assighnment2_ads_Beisen_Ali/src/NoSuchElementExceprion.java
+++ assighnment2_ads_Beisen_Ali/src/NoSuchElementExceprion.java
@@ -1,7 +1,5 @@
-package classes;
-
-public class NoSuchElementException extends RuntimeException {//java util is not allowed, so create class
-    public NoSuchElementException(String message) {//create exceptions
+public class Exception extends RuntimeException {
+    public Exception(String message) {
         super(message);
     }
 }
