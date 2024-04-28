a250031d7db633a5886b4a8dcd9d239283c577ca
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-28 15:32:59 +0500

    Update NewException.java

--- assighnment2_ads_Beisen_Ali/src/NewException.java
+++ assighnment2_ads_Beisen_Ali/src/NewException.java
@@ -1,4 +1,6 @@
 public class NewException extends RuntimeException {
+    /* Constructor to create NewException with an error message
+    */
     public NewException(String message) {
         super(message);
     }
