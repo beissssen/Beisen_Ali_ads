1d5799ac63186919cdb8803baa7ca425f908b374
beissssen <154684787+beissssen@users.noreply.github.com>
2024-04-28 15:28:07 +0500

    Update DataType.java

--- assighnment2_ads_Beisen_Ali/src/DataType.java
+++ assighnment2_ads_Beisen_Ali/src/DataType.java
@@ -1,6 +1,11 @@
 import classes.MyList;
 
 public interface DataType<T> {
+    /** Declaration of a generic interface DataType with type parameter T.
+    * Method to add an element of type T to the data structure,
+    * method to remove and return the last added element of type T from the data structure
+    * and method to return the current size of the data structure are used here
+    */
     void push(T e);
     T pop();
     T get();
